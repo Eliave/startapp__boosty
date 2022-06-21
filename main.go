6fc37a0ffe6ed737fb08c6a84b0ae7af290e3688
ivan_semenov <semenofiv@gmail.com>
2022-06-21 11:25:14 +0500

    Добавил 404 not found Написал неработающую менюшку на index.html подключил папку static

--- main.go
+++ main.go
@@ -1,40 +1,47 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"net/http"
 )
 
-type User struct {
-	name  string
-	age   uint16 //Целое положительное число
-	money float32
-}
+// type User struct {
+// 	name  string
+// 	age   uint16 //Целое положительное число
+// 	money float32
+// }
 
-func (u User) getAllInfo() string {
-	return fmt.Sprint("User name is: ", u.name, ". He is ", u.age, ". And he has money ", u.money, "\n")
-}
+// func (u User) getAllInfo() string {
+// 	return fmt.Sprint("User name is: ", u.name, ". He is ", u.age, ". And he has money ", u.money, "\n")
+// }
 
-func (u *User) setNewName(newName string) {
-	u.name = newName
-}
+// func (u *User) setNewName(newName string) {
+// 	u.name = newName
+// }
 
 func home_page(w http.ResponseWriter, r *http.Request) {
-	bob := User{
-		name: "Bob", age: 24, money: 24.5,
+	//инициализация 404 Not Found
+	if r.URL.Path != "/" {
+		templ_404, _ := template.ParseFiles("static/templates/404.html")
+		templ_404.Execute(w, r)
+		return
 	}
-	templ, _ := template.ParseFiles("templates/index.html")
-	templ.Execute(w, bob)
+	// bob := User{
+	// 	name: "Bob", age: 24, money: 24.5,
+	// }
+	templ, _ := template.ParseFiles("static/templates/index.html")
+	templ.Execute(w, r)
 }
 
 func profile(w http.ResponseWriter, r *http.Request) {
-	bob := User{
-		name: "Bob", age: 24, money: 24.5,
-	}
-	fmt.Fprintf(w, bob.getAllInfo())
-	bob.setNewName("Ivan")
-	fmt.Fprintf(w, bob.getAllInfo())
+	// bob := User{
+	// 	name: "Bob", age: 24, money: 24.5,
+	// }
+	// fmt.Fprintf(w, bob.getAllInfo())
+	// bob.setNewName("Ivan")
+	// fmt.Fprintf(w, bob.getAllInfo())
+	templ, _ := template.ParseFiles("static/templates/profile.html")
+	templ.Execute(w, r)
 }
 
 func handleRequest() {
@@ -42,6 +49,8 @@ func handleRequest() {
 	http.HandleFunc("/", home_page)
 	//Ссылка на профиль
 	http.HandleFunc("/profile/", profile)
+	//инициализация обработчика запроса static
+	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./static"))))
 	//Инициализация localhost с портом 8000
 	http.ListenAndServe(":8000", nil)
 }
